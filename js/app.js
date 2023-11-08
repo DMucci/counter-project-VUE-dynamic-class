df589e1d699116ee2cf16ea094713d728ded0023
Dmucci <dmucci93@gmail.com>
2023-11-08 13:06:13 +0100

    finished

--- js/app.js
+++ js/app.js
@@ -7,9 +7,30 @@ import {
 
 createApp({
   setup() {
-    const message = ref("Hello Vue!");
+    const number = ref(0);
+    const increase = () => {
+      number.value++;
+    };
+    const decrease = () => {
+      number.value--;
+    };
+
+    const getColorCount = () => {
+      if (number.value == 0) {
+        return 'black';
+      } else if (number.value > 0) {
+        return "green";
+      } else 
+      return "red";
+    };
+
+    //OTRA MANERA DE HACERLO SERIA EN EL HTML: :style"{color: counter === 0 ? 'gray' : (counter >0 ? 'green' : 'red')}"   o bien poner :class="{'text-success': counter>0, 'text-danger':counter<0}"
+
     return {
-      message,
+      number,
+      increase,
+      decrease,
+      getColorCount,
     };
   },
 }).mount("#app");
